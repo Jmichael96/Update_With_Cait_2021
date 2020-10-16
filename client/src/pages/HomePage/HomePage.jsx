d4d266263b11c8eddd7775bdc59659aee6bb1570
Jeffrey VanHorn <jeffrey.vanhorn@yahoo.com>
2020-10-16 14:25:32 -0500

    configured intro image section on homepage

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,52 +1,36 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { fetchLifestyle, fetchDevotional, fetchGraphics, fetchWellness } from '../../store/actions/post';
+import { } from '../../store/actions/post';
 
 // styles
+import './homePage.css';
 
 // components
 import isEmpty from '../../utils/isEmpty';
 
-const HomePage = ({ fetchLifestyle, fetchDevotional, fetchGraphics, fetchWellness, post: { loading, lifestylePosts, devotionalPosts, graphicsPosts, wellnessPosts } }) => {
+const HomePage = ({ }) => {
 
-    // fetch all the posts appropriately 
-    // useEffect(() => {
-    //     fetchPostsHandler();
-    // }, []);
-
-    // const fetchPostsHandler = async () => {
-    //     if (!loading && isEmpty(lifestylePosts)) {
-    //         await fetchLifestyle();
-    //     }
-    //     if (!loading && isEmpty(devotionalPosts)) {
-    //         await fetchDevotional();
-    //     }
-    //     if (!loading && isEmpty(graphicsPosts)) {
-    //         await fetchGraphics();
-    //     }
-    //     if (!loading && isEmpty(wellnessPosts)) {
-    //         await fetchWellness();
-    //     }
-    // };
 
     return (
-        <article>
-            <h1>Home Page</h1>
+        <article id="homePageStyles_root">
+            <section id="homePageStyles_introImageWrap">
+                <div id="homePageStyles_introInnerWrap">
+                    <img className="homePageStyles_underlayImgLeft" src={require('../../assets/images/widemountain2.jpg')} />
+                    <img id="homePageStyles_mainImg" src={require('../../assets/images/cait.JPG')} />
+                    <img className="homePageStyles_underlayImgRight" src={require('../../assets/images/widemountain1.jpg')} />
+                </div>
+            </section>
         </article>
     );
 };
 
 HomePage.propTypes = {
-    fetchLifestyle: PropTypes.func.isRequired,
-    fetchDevotional: PropTypes.func.isRequired,
-    fetchGraphics: PropTypes.func.isRequired,
-    fetchWellness: PropTypes.func.isRequired,
-    post: PropTypes.object.isRequired,
+
 };
 
 const mapStateToProps = (state) => ({
     post: state.post
 });
 
-export default connect(mapStateToProps, { fetchLifestyle, fetchDevotional, fetchGraphics, fetchWellness })(HomePage);
+export default connect(mapStateToProps, {})(HomePage);
